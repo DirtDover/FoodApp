a644bfbdb0162f0d8c406bfb460f7670276dabf1
DirtDover <benjaminmbureau@gmail.com>
2023-08-31 16:04:58 +0200

    Modif images + Reset button

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const search = document.querySelector('button');
 const error = document.querySelector('.error');
+const result = document.querySelector('.result_container')
 
 
 async function getFood() {
@@ -8,15 +9,30 @@ async function getFood() {
     const name = document.querySelector('.name_product');
     const grade = document.querySelector('.nutrition_grades');
     const cal = document.querySelector('.result_container .cal');
+    const image = document.querySelector('.result_container img')
+
+    name.innerHTML = '';
+    grade.innerHTML = '';
+    cal.innerHTML = '';
     
     let rep = await fetch(`https://world.openfoodfacts.net/api/v2/product/${barcode}?fields=product_name,nutriscore_data,nutriments,nutrition_grades
     `);
     let reponse = await rep.json();
     const data = reponse
+    console.log(data)
     
 
     if(data.status === 0){
-        error.style.display ='block';
+        error.style.display ='block'; 
+        name.innerHTML = '';
+        grade.innerHTML = '';
+        cal.innerHTML = '';
+        return;
+    } else if (data.code !== barcode) {
+        error.style.display ='block';       
+        name.innerHTML = '';
+        grade.innerHTML = '';
+        cal.innerHTML = '';
         return;
     } else {
         error.style.display = 'none';
@@ -24,8 +40,31 @@ async function getFood() {
         grade.innerHTML =  `Rang ${data.product.nutrition_grades}`;
         cal.innerHTML = `${data.product.nutriments['energy-kcal_100g']} Kcal pour 100g`;
     }
-    
+
+
+        switch(data.product.nutrition_grades){
+
+            case 'a' :
+                image.src ='images/A.svg';
+                break;
+            case 'b' :
+                image.src ='images/B.svg';
+                break;
+            case 'c' :
+                image.src ='images/C.svg';
+                break;
+            case 'd' :
+                image.src ='images/D.svg';
+                break;
+            case 'e' :
+                image.src ='images/E.svg';
+                break;
+            default :
+                image.src = ''
+        }
 };
+
+
 
 
 search.addEventListener('click',(e) => {
